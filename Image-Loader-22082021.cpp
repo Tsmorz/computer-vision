a373844c3c987ca5c6eb8d22f07ebb57c59ea86b
Tsmorz <tonysmoragiewicz@gmail.com>
2021-08-22 20:57:49 +0200

    opencv4 doesn't work

--- Image-Loader-22082021.cpp
+++ Image-Loader-22082021.cpp
@@ -1,7 +1,7 @@
 //Uncomment the following line if you are compiling this code in Visual Studio
 //#include "stdafx.h"
 
-#include <opencv2/opencv.hpp>
+#include <opencv4.hpp>
 #include <iostream>
 
 using namespace cv;
