677a89301438954f27dd07313c2088b2062e4f1d
tsmorz <tonysmoragiewicz@gmail.com>
2021-09-09 22:27:42 -0700

    edges

--- edges.py
+++ edges.py
@@ -14,14 +14,13 @@ img = cv2.GaussianBlur(gray,(7,7),0)
 laplacian = cv2.Laplacian(img,cv2.CV_64F)
 sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)  # x
 sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)  # y
+mag = np.sqrt(sobelx**2 + sobely**2)
+print(mag.shape)
 
-plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
+plt.subplot(1,2,1),plt.imshow(img,cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
-plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
-plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
+
+plt.subplot(1,2,2),plt.imshow(mag,cmap = 'gray')
 plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
 
 plt.show()
