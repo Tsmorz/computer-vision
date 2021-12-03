d507f521d260db1d15f3cfd151c71d99268dcdd2
matinraayai <raayaiardani.m@northeastern.edu>
2021-12-03 12:20:56 -0500

    Better borders, not yet fixed for cast. Visualization is working now.

--- Project 3/project3-cuda-extension/project3_kernel.cu
+++ Project 3/project3-cuda-extension/project3_kernel.cu
@@ -11,42 +11,44 @@
 
 template<typename scalar_t>
 __device__
-scalar_t local_patch_ncc(const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits>& img_src,
-                      const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits>& img_dest,
-                      int src_x,
-                      int src_y,
-                      int dest_x,
-                      int dest_y,
+scalar_t local_patch_ncc(const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits>& left_img,
+                      const torch::PackedTensorAccessor32<scalar_t, 2, torch::RestrictPtrTraits>& right_img,
+                      int left_x,
+                      int left_y,
+                      int right_x,
+                      int right_y,
                       int patch_rad) {
-    int r = img_dest.size(0);
-    int c = img_dest.size(1);
-    int src_patch_x_begin = src_x - patch_rad < 0 ? src_x : patch_rad;
-    int src_patch_x_end = src_x + patch_rad > r ? r - src_x : patch_rad;
-    int src_patch_y_begin = src_y - patch_rad < 0 ? src_y : patch_rad;
-    int src_patch_y_end = src_y + patch_rad > c ? r - src_y : patch_rad;
-    int dest_patch_x_begin = dest_x - patch_rad < 0 ? dest_x : patch_rad;
-    int dest_patch_x_end = dest_x + patch_rad > r ? r - dest_x : patch_rad;
-    int dest_patch_y_begin = dest_y - patch_rad < 0 ? dest_y : patch_rad;
-    int dest_patch_y_end = dest_y + patch_rad > c ? r - dest_y : patch_rad;
+    int r_left = left_img.size(0);
+    int c_left = left_img.size(1);
+    int r_right = right_img.size(0);
+    int c_right = right_img.size(1);
+    int left_patch_x_begin = left_x - patch_rad < 0 ? left_x : patch_rad;
+    int left_patch_x_end = left_x + patch_rad > r_left ? r_left - left_x : patch_rad;
+    int left_patch_y_begin = left_y - patch_rad < 0 ? left_y : patch_rad;
+    int left_patch_y_end = left_y + patch_rad > c_left ? c_left - left_y : patch_rad;
+    int right_patch_x_begin = right_x - patch_rad < 0 ? right_x : patch_rad;
+    int right_patch_x_end = right_x + patch_rad > r_right ? r_right - right_x : patch_rad;
+    int right_patch_y_begin = right_y - patch_rad < 0 ? right_y : patch_rad;
+    int right_patch_y_end = right_y + patch_rad > c_right ? c_right - right_y : patch_rad;
 
-    int patch_x_begin = min(src_patch_x_begin, dest_patch_x_begin);
-    int patch_x_end = min(src_patch_x_end, dest_patch_x_end);
-    int patch_y_begin = min(src_patch_y_begin, dest_patch_y_begin);
-    int patch_y_end = min(src_patch_y_end, dest_patch_y_end);
-    scalar_t src_norm = 0.;
-    scalar_t dest_norm = 0.;
+    int patch_x_begin = min(left_patch_x_begin, right_patch_x_begin);
+    int patch_x_end = min(left_patch_x_end, right_patch_x_end);
+    int patch_y_begin = min(left_patch_y_begin, right_patch_y_begin);
+    int patch_y_end = min(left_patch_y_end, right_patch_y_end);
+    scalar_t left_norm = 0.;
+    scalar_t right_norm = 0.;
     scalar_t ncc = 0.;
     for (int x = -patch_x_begin; x <= patch_x_end; x++) {
     for (int y = -patch_y_begin; y <= patch_y_end; y++) {
-        scalar_t curr_src = img_src[src_x + x][src_y + y];
-        scalar_t curr_dest = img_dest[dest_x + x][dest_y + y];
+        scalar_t curr_src = left_img[left_x + x][left_y + y];
+        scalar_t curr_dest = right_img[right_x + x][right_y + y];
         ncc += curr_src * curr_dest;
-        src_norm += curr_src * curr_src;
-        dest_norm += curr_dest * curr_dest;
+        left_norm += curr_src * curr_src;
+        right_norm += curr_dest * curr_dest;
     }}
-    src_norm = sqrt(src_norm);
-    dest_norm = sqrt(dest_norm);
-    ncc /= (src_norm * dest_norm);
+    left_norm = sqrt(left_norm);
+    right_norm = sqrt(right_norm);
+    ncc /= (left_norm * right_norm);
     return ncc;
 }
 
@@ -69,16 +71,16 @@ void k_disparity_map(const torch::PackedTensorAccessor32<scalar_t, 2, torch::Res
         const int c = left_img.size(1);
         if (i < r && j < c) {
             // Line equation calculation. Image coordinates are inverted
-            const float dest_line_a = f[0][0] * j + f[0][1] * i + f[0][2];
-            const float dest_line_b = f[1][0] * j + f[1][1] * i + f[1][2];
-            const float dest_line_c = f[2][0] * j + f[2][1] * i + f[2][2];
+            const float right_line_a = f[0][0] * j + f[0][1] * i + f[0][2];
+            const float right_line_b = f[1][0] * j + f[1][1] * i + f[1][2];
+            const float right_line_c = f[2][0] * j + f[2][1] * i + f[2][2];
             // Loop invariants
             scalar_t best_match_score = 0.;
-            int best_match_x = -INT32_MAX;
-            int best_match_y = -INT32_MAX;
+            int best_match_x = 0;
+            int best_match_y = 0;
             // image coordinates are inverted
             for (int y = 0; y < r; y++) {
-                int x = static_cast<int>(-(dest_line_a * y + dest_line_c) / dest_line_b);
+                int x = static_cast<int>(-(right_line_a * y + right_line_c) / right_line_b);
                 if (x >= 0 && x < c) {
                     scalar_t curr_match_score = local_patch_ncc(left_img, right_img, i, j, x, y, patch_rad);
                     if (best_match_score < curr_match_score) {
@@ -88,8 +90,9 @@ void k_disparity_map(const torch::PackedTensorAccessor32<scalar_t, 2, torch::Res
                     }
                 }
             }
-            dis_map[i][j][0] = best_match_x - i;
-            dis_map[i][j][1] = best_match_y - j;
+            // Again, invert to reflect 
+            dis_map[i][j][0] = best_match_y - j;
+            dis_map[i][j][1] = best_match_x - i;
             accuracy_map[i][j] = best_match_score;
             dense_matches[i][j][0] = best_match_x;
             dense_matches[i][j][1] = best_match_y;
